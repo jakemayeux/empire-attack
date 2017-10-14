a9dbe19e3079478c7376cbae2dac00c2f64a1bea
Patrick Jimenez <pjimenezgicko@gmail.com>
2017-10-14 18:58:57 -0400

    hey

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ class Game {
   constructor(){
     this.width = 100
     this.height = 100
-    this.map = new Array()
+    this.map = [] 
     this.id = shortid.generate()
     this.maxPlayers = 30
     this.players = 0
@@ -20,11 +20,45 @@ class Game {
         this.map[x][y] = new Tile()
       }
     }
+	  this.genLake(1)
   }
+  
+  genLake(i){
+	  if(i<1) return
 
-  addPlayer(socket){
-    console.log('adding ' + socket.id + ' to game ' + game.id)
-    socket.emit('test', 'asdf')
+	  let changes = []
+    for(let x = 0; x < this.width; x++){
+		  for(let y = 0; y < this.width; y++){
+				let wn = 0;
+				for(let t of this.getNeighbors(x,y)){
+					if(t.type == TILE.LAKE) wn++
+				}
+
+				let rand = Math.random() * wm
+				if(rand > .3){
+					changes.push({x:x,y:y})
+				}
+	    }
+	  }
+		for(let t of changes){
+			this.map[t.x][t.y].type = TILE.LAKE
+		}
+	}
+		
+
+	getNeighbors(x,y){
+		let ret = []
+		if(x > 0) ret.push(this.map[x-1][y])
+		if(y > 0) ret.push(this.map[x][y-1])
+		if(x < this.width - 1) ret.push(this.map[x+1][y])
+		if(y < this.height - 1) ret.push(this.map[x][y+1])
+		return ret
+	}
+
+	addPlayer(socket){
+  	console.log('adding ' + socket.id + ' to game ' + game.id)
+  	socket.emit('test', 'asdf')
+		socket.emit('map', this.map)
   }
 }
 
@@ -32,7 +66,8 @@ const TILE = {
   CITY: 'city',
   COIN: 'coin',
   ROCK: 'rock',
-  WATER: 'water',
+  RIVER: 'river',
+  LAKE: 'lake',
   BLANK: 'blank'
 }
 
@@ -44,7 +79,7 @@ class Tile {
     }else if(rand < .9){
       this.type = TILE.ROCK
     }else{
-      this.type = TILE.WATER
+      this.type = TILE.LAKE
     }
   }
 }
